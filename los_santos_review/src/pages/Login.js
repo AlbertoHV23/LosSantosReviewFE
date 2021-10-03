d3b275dc66e06ae6694236c97ffdd0207d5dae71
AlbertoHV23 <42360698+AlbertoHV23@users.noreply.github.com>
2021-10-03 01:54:34 -0500

    Material UI & Bootstrap
    Adding Material UI & Bootstrap to project

--- los_santos_review/src/pages/Login.js
+++ los_santos_review/src/pages/Login.js
@@ -1,14 +1,28 @@
 import React from "react";
+//My components
 import Logo from "../components/common/logo";
+import Title from "../components/login/Title";
+import LoginForm from "../components/login/Form";
+
+//Material UI & Bootstrap components
 import Button from "@mui/material/Button";
+import { Grid } from "@mui/material";
+
+import "./Login.scss";
 
 function Login() {
   return (
     <>
-      <div>Login</div>
-      <Logo></Logo>
-      <Button variant="contained">Hello World</Button>
-      <Button variant="outlined">Primary</Button>
+      <Grid container>
+        <Grid item xs={4} md={6}>
+          <Logo />
+          <Title title="Login" />
+          <LoginForm />
+        </Grid>
+        <Grid item xs={4} md={6}>
+          <Button variant="outlined">Log in</Button>
+        </Grid>
+      </Grid>
     </>
   );
 }
