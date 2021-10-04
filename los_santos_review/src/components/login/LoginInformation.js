2eb0ef42e53b5d102f3414725a41a5f030a0d5cd
AlbertoHV23 <42360698+AlbertoHV23@users.noreply.github.com>
2021-10-03 23:14:42 -0500

    Login - Titles Color
    Change color in login.js

---
+++ los_santos_review/src/components/login/LoginInformation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const imagen = './Login.svg';
+function LoginInformation() {
+    return (
+        <>
+        <img className="svg" src='https://firebasestorage.googleapis.com/v0/b/camppus-224af.appspot.com/o/Login.svg?alt=media&token=71308044-38d8-44ee-9724-e8a1e6f66a4b' alt="" />
+        </>
+      );
+}
+
+export default LoginInformation;
