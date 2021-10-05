378d084618b9e5011cf5f727a2b35a1c2073a2d1
Romr <rome_gs@hotmail.com>
2021-10-05 03:29:27 -0500

    Main
    Local logo asset  src successful

--- los_santos_review/src/components/common/logo.js
+++ los_santos_review/src/components/common/logo.js
@@ -1,9 +1,8 @@
 import React from "react";
-// import logo from 'https://uanledu.sharepoint.com/sites/FCFMLossantos/Documentos%20compartidos/Programacion%20Web%202/Los%20Santos.png' // relative path to image 
 
 function Logo() {
     return (
-        <img className="logo" src= 'https://uanledu.sharepoint.com/sites/FCFMLossantos/Documentos%20compartidos/Programacion%20Web%202/IMG/LSRlogo.png' />
+        <img className="logo" src= {`${process.env.PUBLIC_URL}/assets/img/LSRlogo.png`} />
      );
 }
 
---
+++ los_santos_review/src/pages/main/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+//My components
+import Logo from "../../components/common/logo";
+import Title from "../../components/common/Title";
+import LoginForm from "../../components/login/Form";
+
+//Material UI & Bootstrap components
+
+function Main() {
+    return (
+      <>
+        <Logo/>
+      </>
+    );
+  }
+  
+  export default Main;
