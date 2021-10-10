147a2ada0008fdac475f03a3ad7f5a4d53a076f8
AlbertoHV23 <42360698+AlbertoHV23@users.noreply.github.com>
2021-10-10 00:37:45 -0500

    Profile Done...

--- los_santos_review/src/components/Account/profile/ProfileForm.js
+++ los_santos_review/src/components/Account/profile/ProfileForm.js
@@ -7,28 +7,48 @@ function ProfileForm() {
   return (
     <>
       <Form className="form-login">
-        <Form.Group className="mb-4" controlId="formBasicEmail">
-          <Form.Label className="text-blue">Email:</Form.Label>
+      <Form.Group className="mb-4" controlId="formBasicPassword">
+          <Form.Label className="text-blue">Name(s):</Form.Label>
           <Form.Control
-            type="email"
-            placeholder="example@email.com"
-            className="input-"
+            type="text"
+            className="input-profile"
+            value ="Alberto"
           />
         </Form.Group>
 
         <Form.Group className="mb-4" controlId="formBasicPassword">
-          <Form.Label className="text-blue">Password:</Form.Label>
+          <Form.Label className="text-blue">Surnames(s):</Form.Label>
           <Form.Control
-            type="password"
-            placeholder="********"
+            type="text"
             className="input-profile"
+            value ="Hernadez"
           />
         </Form.Group>
+
+        <Form.Group className="mb-4" controlId="formBasicPassword">
+          <Form.Label className="text-blue">Username):</Form.Label>
+          <Form.Control
+            type="text"
+            className="input-profile"
+            value ="Alberthor"
+          />
+        </Form.Group>
+        
+        <Form.Group className="mb-4" controlId="formBasicEmail">
+          <Form.Label className="text-blue">Email:</Form.Label>
+          <Form.Control
+            type="email"
+            className="input-profile"
+            value = "albertho0.hdzv@gmail.com"
+          />
+        </Form.Group>
+
+      
 
         
       
 
-        <Button variant="primary" className="btn-login mb-4">Login</Button>{' '}
+        <Button variant="primary" className="btn-login mb-4">Save</Button>{' '}
 
      
       </Form>
