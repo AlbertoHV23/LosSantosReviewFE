0e3f7f73be574ee4c169eb8236bf5d5d7817d731
Rome1317 <42395292+Rome1317@users.noreply.github.com>
2021-10-20 21:54:54 -0500

    Company Form Styled

---
+++ los_santos_review/src/components/company-form/Form.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import Button from "@restart/ui/esm/Button";
+import EditIcon from '@mui/icons-material/Edit';
+import BootstrapDate from "../common/Date-Picker";
+import TimePicker from 'react-bootstrap-time-picker';
+import Title from "../common/Title";
+
+function FormCompany() {;
+    return (
+        <>
+
+        <Row>
+            <Title title="Company" class = "title"/> 
+            <hr></hr>
+        </Row>
+
+            <Form>
+                
+                <Row className="mb-3 mt-3">
+                    
+                    <Form.Group as={Col} controlId="formGridSubtitlle">
+                      <Form.Label>Company Name</Form.Label>
+                      <Form.Control placeholder="Enter Company Name" />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formFile" className="mb-3">
+                        <Form.Label>Company Logo</Form.Label>
+                        <Form.Control type="file" />
+                    </Form.Group>
+                </Row>
+
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridTitle">
+                      <Form.Label>Released Date</Form.Label>
+                      <BootstrapDate/>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridState">
+                         <Form.Label>Country</Form.Label>
+                         <Form.Select defaultValue="Choose Content Company">
+                            <option hidden selected>Choose Company Country</option>
+                         </Form.Select>
+                     </Form.Group>
+                </Row>
+
+                <div className="right">
+                    <Button variant="primary" className="button submit margin" type="submit">SUBMIT</Button>
+                    <Button variant="primary" className="button submit delete" type="submit">DELETE</Button>
+                </div>
+            </Form>
+        </>
+     );
+}
+
+export default FormCompany;
---
+++ los_santos_review/src/pages/company-form/CompanyForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+//My components
+import Title from "../../components/common/Title";
+import Header from "../../components/main/Header";
+import FormCompany from "../../components/company-form/Form";
+import Footer from "../../components/main/Footer";
+import { Container } from "react-bootstrap";
+
+import "../main/Main.scss";
+
+
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"></link>
+
+//Material UI & Bootstrap components
+
+function CompanyForm() {
+    return (
+      <>
+        <Header/>
+        <Container>
+            <FormCompany/>
+        </Container>
+        <Footer/>
+      </>
+    );
+  }
+  
+  export default CompanyForm;
