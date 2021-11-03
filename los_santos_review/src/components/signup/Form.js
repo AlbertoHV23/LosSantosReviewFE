585694083e17aaf6a9a53b2b079d7b7cf4876c22
Rome1317 <42395292+Rome1317@users.noreply.github.com>
2021-11-02 22:50:32 -0600

    Sign Up Post connection

--- los_santos_review/src/components/signup/Form.js
+++ los_santos_review/src/components/signup/Form.js
@@ -8,6 +8,39 @@ import Select from 'react-select'
 function Form_() {
 
   let roles = [];
+  let role,username, name, lastName, email, password;
+
+  const RoleHandler = (e) =>{
+    role = e.value 
+    console.log(role)
+  }
+
+  const UsernameHandler = (e) =>{
+    username = e.target.value
+    console.log(username)
+  }
+
+  const NameHandler = (e) =>{
+    name = e.target.value
+    console.log(name)
+  }
+
+  const SurnameHandler = (e) =>{
+    lastName = e.target.value
+    console.log(lastName)
+  }
+  
+  
+  const EmailHandler = (e) =>{
+    email = e.target.value
+    console.log(email)
+  }
+  
+
+  const PasswordHandler = (e) =>{
+    password = e.target.value
+    console.log(password)
+  }
 
   const getRoles = (e) =>{
     axios.get(`https://lossantos-api.herokuapp.com/api/role`,{roles})
@@ -43,20 +76,64 @@ function Form_() {
 
   getRoles()
 
+  
+  const Submit = (e) =>{
+    axios.post(`https://lossantos-api.herokuapp.com/api/user/`,{
+      name,
+      lastName,
+      username,
+      email,
+      password,
+      role
+    })
+    .then(res => {
+
+      //const {}
+    
+      window.localStorage.setItem(
+        'user' , JSON.stringify(res)
+      )
+      const user = JSON.parse(localStorage.getItem('user'))
+      console.log(user);
+
+      window.location = "main"
+
+    })
+    .catch(err => {
+      const errorMsg =JSON.parse(err.request.response) ;
+
+      if(errorMsg.errors != null){
+        errorMsg.errors.forEach(e => {
+      
+          alert(e.msg);
+  
+        });
+      }
+      else{
+        alert("Something went wrong")
+      }
+
+
+    })
+
+  }
+
   return (
     <>
       <Form className="form-signup">
         <Form.Group className="mb-4" controlId="formBasicDropdown">
           <Form.Label className="text-pink">Rol:</Form.Label>
-          <Select options={roles} />
+          <Select options={roles} onChange={RoleHandler} />
+          
         </Form.Group>
 
         <Form.Group className="mb-4" controlId="formBasicEmail">
-          <Form.Label className="text-pink">Name(s):</Form.Label>
+          <Form.Label className="text-pink">Username:</Form.Label>
           <Form.Control
             type="text"
             placeholder=""
             className="input-signup"
+            onChange={UsernameHandler}
           />
         </Form.Group>
 
@@ -66,15 +143,17 @@ function Form_() {
             type="text"
             placeholder=""
             className="input-signup"
+            onChange={NameHandler}
           />
         </Form.Group>
 
         <Form.Group className="mb-4" controlId="formBasicSurnames">
-          <Form.Label className="text-pink">Surname(s):</Form.Label>
+          <Form.Label className="text-pink">Lastname(s):</Form.Label>
           <Form.Control
             type="text"
             placeholder=""
             className="input-signup"
+            onChange={SurnameHandler}
           />
         </Form.Group>
 
@@ -84,6 +163,7 @@ function Form_() {
             type="email"
             placeholder="example@email.com"
             className="input-signup"
+            onChange={EmailHandler}
           />
         </Form.Group>
 
@@ -93,10 +173,11 @@ function Form_() {
             type="password"
             placeholder="********"
             className="input-signup"
+            onChange={PasswordHandler}
           />
         </Form.Group>
 
-        <Button variant="primary" className="btn-signup mb-4">
+        <Button variant="primary" className="btn-signup mb-4" onClick={Submit}>
           Sign Up
         </Button>{" "}
 
