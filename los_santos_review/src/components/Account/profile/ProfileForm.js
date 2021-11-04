c810cc1344d41daa173c7f7800fe87f09f39e3dd
AlbertoHV23 <42360698+AlbertoHV23@users.noreply.github.com>
2021-11-03 23:49:39 -0600

    Update profile form

--- los_santos_review/src/components/Account/profile/ProfileForm.js
+++ los_santos_review/src/components/Account/profile/ProfileForm.js
@@ -1,17 +1,72 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
+import axios from "axios";
+
 
 function ProfileForm() {
+  let  username,name,lastname,email,role,UID;
+  const GetSession = (e) =>{
+    window.localStorage.getItem('user')
+    const user = JSON.parse(localStorage.getItem('user'))
+    console.log(user);
+    username = user.data.newUser.username
+    name = user.data.newUser.name
+    lastname = user.data.newUser.lastName
+    email = user.data.newUser.email
+    role = user.data.newUser.role
+    UID = user.data.newUser.uid
+    console.log(UID)
+  }
+  const NameHandler = (e) =>{
+    name = e.target.value
+  }
+
+  const LastNameHandler = (e) =>{
+    lastname = e.target.value
+  }
+
+
+  const Submit = (e) =>{
+    axios.put(`https://lossantos-api.herokuapp.com/api/user/`+ UID,{
+      name,
+      lastname
+    })
+    .then(res => {
+      console.log(res.data)
+
+    })
+    .catch(err => {
+      const errorMsg =JSON.parse(err.request.response) ;
+
+      if(errorMsg.errors != null){
+        errorMsg.errors.forEach(e => {
+      
+          alert(e.msg);
+  
+        });
+      }
+      else{
+        alert("Wrong")
+      }
+
+
+    })
+
+  }
+
+  GetSession()
+
   return (
     <>
       <Form className="form-login">
       <Form.Group className="mb-4" controlId="formBasicPassword">
-          <Form.Label className="text-blue">Name(s):</Form.Label>
+          <Form.Label className="text-blue" >Name(s):</Form.Label >
           <Form.Control
             type="text"
             className="input-login"
-            value ="Alberto"
+            placeholder = {name}
+            onChange= {NameHandler}
           />
         </Form.Group>
 
@@ -20,7 +75,9 @@ function ProfileForm() {
           <Form.Control
             type="text"
             className="input-login"
-            value ="Hernadez"
+            placeholder ={lastname}
+            onChange= {LastNameHandler}
+
           />
         </Form.Group>
 
@@ -29,7 +86,7 @@ function ProfileForm() {
           <Form.Control
             type="text"
             className="input-login"
-            value ="Alberthor"
+            placeholder ={username}
           />
         </Form.Group>
         
@@ -38,7 +95,7 @@ function ProfileForm() {
           <Form.Control
             type="email"
             className="input-login"
-            value = "albertho0.hdzv@gmail.com"
+            placeholder ={email}
           />
         </Form.Group>
 
@@ -47,7 +104,8 @@ function ProfileForm() {
         
       
 
-        <Button variant="primary" className="btn-login mb-4">Save</Button>{' '}
+        <Button variant="primary" className="btn-login mb-4"
+        onClick= {Submit}>Save</Button>
 
      
       </Form>
