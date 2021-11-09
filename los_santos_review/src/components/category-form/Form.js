0111a7bfb2b51adc62e204181439c093ee15da22
AlbertoHV23 <42360698+AlbertoHV23@users.noreply.github.com>
2021-11-08 18:17:41 -0600

    Category & Classification submit

--- los_santos_review/src/components/category-form/Form.js
+++ los_santos_review/src/components/category-form/Form.js
@@ -9,6 +9,85 @@ import axios from "axios";
 
 
 function FormCategory() {
+    let Name; //Category
+    let ClassName,ClassDescription; //Classification
+    let username, name, lastName, email, role, uid, token; // SESSION VARS
+
+  const GetSession = (e) => {
+    window.localStorage.getItem("user");
+    const user = JSON.parse(localStorage.getItem("user"));
+    console.log(user);
+    username = user.data.newUser.username;
+    name = user.data.newUser.name;
+    lastName = user.data.newUser.lastName;
+    email = user.data.newUser.email;
+    role = user.data.newUser.role;
+    uid = user.data.newUser.uid;
+    token = user.data.token;
+
+    console.log(token);
+  };
+
+  GetSession()
+
+    const CategoryHandler = (e) =>{ Name = e.target.value }
+    const ClassficationHandler = (e) =>{ ClassName = e.target.value}
+    const ClasDescriptionHandler = (e) =>{ ClassDescription = e.target.value; console.log(ClassDescription)}
+
+    const CategorySubmit = (e) => {
+        axios({
+          method: "post",
+          url: `https://lossantos-api.herokuapp.com/api/category/`,
+          headers: {
+            "x-token": token
+          },
+          data: {
+            'name': Name
+          },
+        })
+          .then((res) => {
+            console.log(res.data);
+          })
+          .catch((err) => {
+            const errorMsg = JSON.parse(err.request.response);
+    
+            if (errorMsg.errors != null) {
+              errorMsg.errors.forEach((e) => {
+                alert(e.msg);
+              });
+            } else {
+              alert("Something went wrong, please try again.");
+            }
+          });
+      };
+    const ClassficationSubmit = (e) => {
+        axios({
+          method: "post",
+          url: `https://lossantos-api.herokuapp.com/api/classification/`,
+          headers: {
+            "x-token": token
+          },
+          data: {
+            'name': ClassName,
+            'description': ClassDescription,
+          },
+        })
+          .then((res) => {
+            console.log(res.data);
+          })
+          .catch((err) => {
+            const errorMsg = JSON.parse(err.request.response);
+    
+            if (errorMsg.errors != null) {
+              errorMsg.errors.forEach((e) => {
+                alert(e.msg);
+              });
+            } else {
+              alert("Something went wrong, please try again.");
+            }
+          });
+      };
+ 
     return (
         <>
 
@@ -22,14 +101,17 @@ function FormCategory() {
 
                     <Form.Group as={Col} controlId="formGridState">
                       <Form.Label>Name</Form.Label>
-                      <Form.Control placeholder="Enter Category Name"></Form.Control>
+                      <Form.Control placeholder="Enter Category Name"
+                      onChange={CategoryHandler}
+                      ></Form.Control>
                     </Form.Group>
                 </Row>
                 
 
                 <div className="right">
-                    <Button variant="primary" className="button submit margin" type="submit">SAVE</Button>
-                    <Button variant="primary" className="button submit delete" type="submit">DELETE</Button>
+                      
+                    <Button variant="primary" className="button submit margin"   onClick={CategorySubmit} >SAVE</Button>
+                    <Button variant="primary" className="button submit delete">DELETE</Button>
                 </div>
             </Form>
 
@@ -56,8 +138,8 @@ function FormCategory() {
                 
 
                 <div className="right">
-                    <Button variant="primary" className="button submit margin" type="submit">SAVE</Button>
-                    <Button variant="primary" className="button submit delete" type="submit">DELETE</Button>
+                    <Button variant="primary" className="button submit margin" >SAVE</Button>
+                    <Button variant="primary" className="button submit delete" >DELETE</Button>
                 </div>
             </Form>
 
@@ -70,19 +152,19 @@ function FormCategory() {
                 <Row className="mb-3 mt-3"> 
                     <Form.Group as={Col} controlId="formGridState">
                       <Form.Label>Classification Name</Form.Label>
-                      <Form.Control placeholder="Enter Classification Name"></Form.Control>
+                      <Form.Control placeholder="Enter Classification Name" onChange ={ClassficationHandler}></Form.Control>
                     </Form.Group>
 
                     <Form.Group as={Col} className="mb-3" controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Description</Form.Label>
-                        <Form.Control as="textarea" rows={3} />
+                        <Form.Control as="textarea" rows={3} onChange = {ClasDescriptionHandler} />
                     </Form.Group>
                 </Row>
                 
 
                 <div className="right">
-                    <Button variant="primary" className="button submit margin" type="submit">SAVE</Button>
-                    <Button variant="primary" className="button submit delete" type="submit">DELETE</Button>
+                    <Button variant="primary" className="button submit margin" onClick={ClassficationSubmit}>SAVE</Button>
+                    <Button variant="primary" className="button submit delete" >DELETE</Button>
                 </div>
             </Form>
         </>
